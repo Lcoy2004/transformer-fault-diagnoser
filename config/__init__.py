ce94199a68c42969c09f5e38259b0cd564413571
Lcoy <1498127009@qq.com>
2026-03-11 18:15:40 +0800

    更新入口文件

--- config/__init__.py
+++ config/__init__.py
@@ -1,5 +1,20 @@
+# config/__init__.py
+"""
+配置模块：包含日志配置和通知功能
+"""
+
 from .logging import setup_logging
 from .notification import (
-    notify, get_notification, get_notification_with_time,
+    notify,
+    get_notification,
+    get_notification_with_time,
     clear_notification
 )
+
+__all__ = [
+    'setup_logging',
+    'notify',
+    'get_notification',
+    'get_notification_with_time',
+    'clear_notification'
+]
